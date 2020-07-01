642cf5496d0a9b7405e4d2e002f8799993ab15ad
samlmcewan <samlmcewan@gmail.com>
2020-07-02 00:52:51 +0100

    Connected to soundcloud API

---
+++ javascript/main.js
@@ -0,0 +1,24 @@
+// search	
+
+
+
+// soundcloud API
+SC.initialize({
+  client_id: 'cd9be64eeb32d1741c17cb39e41d254d'
+});
+
+// find all tracks with the genre 'punk' that have a tempo greater than 120 bpm.
+SC.get('/tracks', {
+  genres: 'punk', bpm: { from: 120 }
+}).then(function(tracks) {
+  console.log(tracks);
+});
+
+
+
+// display cards
+
+
+
+
+// add to playlist and play

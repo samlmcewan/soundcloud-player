c6b7fd7a0db3e189e6bf19d1ea5873af40dfe174
samlmcewan <samlmcewan@gmail.com>
2020-07-02 17:09:17 +0100

    added click event to playlist button

--- javascript/main.js
+++ javascript/main.js
@@ -15,10 +15,10 @@ SoundCloudAPI.init();
 SoundCloudAPI.getTrack = function(inputValue) {
 	// find all tracks of query
 	SC.get('/tracks', {
-	   q: inputValue
+	   q: "ailsa and the seahorses"
 	}).then(function(tracks) {
 	  console.log(tracks);
-	  SoundCloudAPI.renderTracks();
+	  SoundCloudAPI.renderTracks(tracks);
 	});
 }
 
@@ -26,7 +26,9 @@ SoundCloudAPI.getTrack();
 
 
 // display cards
-SoundCloudAPI.renderTracks = function() {
+SoundCloudAPI.renderTracks = function(tracks) {
+
+	tracks.forEach(function(track) { 
 
 	// card
 	let card = document.createElement("div");
@@ -43,7 +45,7 @@ SoundCloudAPI.renderTracks = function() {
 
 	let image_img = document.createElement("img");
 	image_img.classList.add("image_img");
-	image_img.src = "http://www.placekitten.com/290/290";
+	image_img.src = track.artwork_url || "https://ae01.alicdn.com/kf/Hf48864be2b3d438595d712f744c88f678/Aquarium-Luminous-Seahorse-ornament-Glow-in-Dark-Landscaping-silicone-sea-horse-Glowing-fish-tank-decoration-hippocampus.jpg_640x640.jpg";
 
 	image.appendChild(image_img)
 
@@ -55,31 +57,53 @@ SoundCloudAPI.renderTracks = function() {
 
 	let header =  document.createElement("div");
 	header.classList.add("header");
-	header.innerHTML = "<a href='#' target='_blank'>'Science Vs. Romance'</a>";
+	header.innerHTML = "<a href='" + track.permalink_url + "' target='_blank'>" + track.title + "</a>";
+
 
 	content.appendChild(header);
 
 
 	// Button
-	let addIcon = document.createElement("div");
-	addIcon.classList.add("ui");
-	addIcon.classList.add("bottom");
-	addIcon.classList.add("attached");
-	addIcon.classList.add("button");
-	addIcon.classList.add("js-button");
+	let button = document.createElement("div");
+	button.classList.add("ui");
+	button.classList.add("bottom");
+	button.classList.add("attached");
+	button.classList.add("button");
+	button.classList.add("js-button");
 
-	card.appendChild(addIcon);
+	card.appendChild(button);
 
 	let iconTing = document.createElement("i");
 	iconTing.classList.add("add");
 	iconTing.classList.add("icon");
 
-	addIcon.appendChild(iconTing);
+	button.appendChild(iconTing);
 
 	let spanner = document.createElement("span");
 	spanner.innerHTML = "Add to playlist";
-	addIcon.appendChild(spanner);
+	button.appendChild(spanner);
+
+	button.addEventListener('click', function(){
+		console.log("click!!!");
+
+	});
+
+
+
+	});
+
+	
 }
+
+SC.oEmbed('https://soundcloud.com/forss/flickermood', {
+  auto_play: true
+}).then(function(embed){
+  console.log('oEmbed response: ', embed);
+
+  const sideBar = document.querySelector(".js-playlist");
+  sideBar.innerHTML = embed.html;
+
+});
 
 
 
